190e05d58962b0c9a42e3bbe0736fdfe1efc2c06
Renê Berredo <reneberredo.a@gmail.com>
2026-07-29 22:40:03 -0300

    Registrar models no admin com list_display e filtros

--- financas/admin.py
+++ financas/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import TipoReceita, TipoDespesa, Receita, Despesa
 
-# Register your models here.
+admin.site.register(TipoReceita)
+admin.site.register(TipoDespesa)
+
+@admin.register(Receita)
+class ReceitaAdmin(admin.ModelAdmin):
+    list_display = ('descricao', 'valor', 'data', 'tipo', 'usuario')
+    list_filter = ('tipo', 'usuario', 'data')
+    search_fields = ('descricao',)
+
+@admin.register(Despesa)
+class DespesaAdmin(admin.ModelAdmin):
+    list_filter = ('descricao', 'valor', 'data', 'tipo', 'usuario')
+    list_filter = ('tipo', 'usuario', 'data')
+    search_fields  = ('descricao',)
