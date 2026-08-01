468460c66fb6368e7ba826c2822af8a0d0b66d78
Renê Berredo <reneberredo.a@gmail.com>
2026-08-01 18:09:43 -0300

    Adicionado sistema de login e logout customizado

--- financas/urls.py
+++ financas/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('receitas/', views.lista_receitas, name='lista_receitas'),
-    path('despesas/', views.lista_despesas, name='lista_despesas')
+    path('despesas/', views.lista_despesas, name='lista_despesas'),
+    path('login/', auth_views.LoginView.as_view(template_name='financas/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
