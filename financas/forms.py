90a4cbd6d51c562431a03eac793cc438101cbbf0
Renê Berredo <reneberredo.a@gmail.com>
2026-08-26 15:52:33 -0300

    Adicionado o date picker e formatacao brasileira de datas

--- financas/forms.py
+++ financas/forms.py
@@ -5,9 +5,15 @@ class ReceitaForm(forms.ModelForm):
     class Meta:
         model = Receita
         fields = ['tipo', 'valor', 'descricao', 'data']
+        widgets = {
+            'data': forms.DateInput(attrs={'type': 'date'}, format='%Y-%m-%d')
+        }
 
 class DespesaForm(forms.ModelForm):
     class Meta:
         model = Despesa
         fields = ['tipo', 'valor', 'descricao', 'data']
-        
+        widgets = {
+                    'data': forms.DateInput(attrs={'type': 'date'}, format='%Y-%m-%d')
+                }
+       
