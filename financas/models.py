bf006e99b8a5d14a24177b31741713b36ab7b49a
Renê Berredo <reneberredo.a@gmail.com>
2026-08-27 10:23:27 -0300

    Adicionado o campo de parcelamento ao Model Despesa

--- financas/models.py
+++ financas/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
+
 
 class TipoReceita(models.Model):
     nome = models.CharField(max_length=100)
@@ -31,6 +33,10 @@ class Despesa(models.Model):
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     descricao = models.CharField(max_length=255, blank=True)
     data = models.DateField()
+    parcelado = models.BooleanField(default=False)
+    parcela_atual = models.PositiveIntegerField(null=True, blank=True)
+    total_parcelas = models.PositiveIntegerField(null=True, blank=True)
+    grupo_parcelamento = models.UUIDField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.descricao} - R$ {self.valor}'
